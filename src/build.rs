57eb01ab8cdb9fdbd3eb87ad26eb9422a8e6b720
zhanglirich <zhanglirich@didichuxing.com>
2018-08-28 15:49:08 +0800

    code clean: add cargo:rerun-if-changed to suppress recompiling the whole project

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,10 @@
 mod _constants;
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/build.rs");
+    println!("cargo:rerun-if-changed=src/_constants/lz_roid_array.rs");
+    println!("cargo:rerun-if-changed=src/_constants/mtf_array.rs");
+
     _constants::lz_roid_array::generate();
     _constants::mtf_array::generate();
 }
