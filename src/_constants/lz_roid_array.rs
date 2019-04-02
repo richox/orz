db71821cf2070aa1befb830559a5f111314b064d
yuge.zl <yuge.zl@alibaba-inc.com>
2019-04-02 14:33:25 +0800

    code clean: simplified generate roid extra bits procedure

--- src/_constants/lz_roid_array.rs
+++ src/_constants/lz_roid_array.rs
@@ -3,39 +3,22 @@
 use std::io::Write;
 
 pub const LZ_MF_BUCKET_ITEM_SIZE: usize = 2046;
-pub const LZ_ROID_SIZE: usize = 22;
+pub const LZ_ROID_SIZE: usize = 20;
 
 pub fn generate() {
-    let robitlens = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, /* ... */];
     let fenc_dest_path = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("LZ_ROID_ENCODING_ARRAY.txt");
     let fdec_dest_path = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("LZ_ROID_DECODING_ARRAY.txt");
     let mut fenc = std::io::BufWriter::new(std::fs::File::create(&fenc_dest_path).unwrap());
     let mut fdec = std::io::BufWriter::new(std::fs::File::create(&fdec_dest_path).unwrap());
 
-    write!(fenc, "[").unwrap();
-    write!(fdec, "[").unwrap();
+    let get_extra_bit_len = |i: usize| -> usize {
+        return i / 2;
+    };
 
-    let mut match_index = 0;
-    let mut current_roid = 0;
-    while match_index < LZ_MF_BUCKET_ITEM_SIZE {
-        let robitlen = if current_roid < robitlens.len() {
-            robitlens[current_roid]
-        } else {
-            robitlens[robitlens.len() - 1]
-        };
-        write!(fdec, "({}, {}), ", match_index, robitlen).unwrap();
-
-        for current_rest_bits in 0 .. (1 << robitlen) {
-            if match_index < LZ_MF_BUCKET_ITEM_SIZE {
-                write!(fenc, "({}, {}, {}), ", current_roid, robitlen, current_rest_bits).unwrap();
-                match_index += 1;
-            }
-        }
-        current_roid += 1;
-    }
-    current_roid += current_roid % 2;
-
-    write!(fenc, "]").unwrap();
-    write!(fdec, "]").unwrap();
-    assert_eq!(current_roid, LZ_ROID_SIZE);
+    let encs = super::generate_extra_bit_codes_enc(LZ_MF_BUCKET_ITEM_SIZE, &get_extra_bit_len);
+    let decs = super::generate_extra_bit_codes_dec(LZ_MF_BUCKET_ITEM_SIZE, &get_extra_bit_len);
+    write!(fenc, "{:?}", encs).unwrap();
+    write!(fdec, "{:?}", decs).unwrap();
+    assert_eq!(encs.len(), LZ_MF_BUCKET_ITEM_SIZE);
+    assert_eq!(decs.len(), LZ_ROID_SIZE);
 }
--- src/_constants/mod.rs
+++ src/_constants/mod.rs
@@ -1,2 +1,34 @@
 pub mod lz_roid_array;
 pub mod mtf_array;
+
+fn generate_extra_bit_codes_enc(count: usize, get_extra_bit_len: &Fn(usize) -> usize) -> Vec<(usize, usize, usize)> {
+    let mut encs = vec![];
+    let mut base = 0;
+    let mut current_id = 0;
+
+    while base < count {
+        let bit_len = get_extra_bit_len(current_id);
+        for rest_bits in 0 .. (1 << bit_len) {
+            if base < count {
+                encs.push((current_id, bit_len, rest_bits));
+                base += 1;
+            }
+        }
+        current_id += 1;
+    }
+    return encs;
+}
+
+fn generate_extra_bit_codes_dec(count: usize, get_extra_bit_len: &Fn(usize) -> usize) -> Vec<(usize, usize)> {
+    let mut decs = vec![];
+    let mut base = 0;
+    let mut current_id = 0;
+
+    while base < count {
+        let bit_len = get_extra_bit_len(current_id);
+        decs.push((base, bit_len));
+        current_id += 1;
+        base += 1 << bit_len;
+    }
+    return decs;
+}
